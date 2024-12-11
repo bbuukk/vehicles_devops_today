b71c30599c23d77bdd860ada82d4ab439d11b0bd
Bohdan Buchok <bohdanbuchok@gmail.com>
2024-12-11 16:33:34 +0200

    feat: introduce fetch function and types for vehicle models

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,11 +1,27 @@
-import { VehicleResponse } from "@/types";
+import { Vehicle, ApiResponse, VehicleModel } from "@/types";
+import { modelsForMakeIdYearRequestParams } from "@/types";
 
-export async function getVehicles(): Promise<VehicleResponse> {
+async function getVehicles(): Promise<ApiResponse<Vehicle>> {
   const res = await fetch(
     "https://vpic.nhtsa.dot.gov/api/vehicles/GetMakesForVehicleType/car?format=json",
   );
 
-  const data: VehicleResponse = await res.json();
+  const data = await res.json();
 
   return data;
 }
+
+//TODO: introduce types to it
+async function getModelsForMakeIdYear({
+  makeId,
+  year,
+}: modelsForMakeIdYearRequestParams): Promise<ApiResponse<VehicleModel>> {
+  const res = await fetch(
+    `https://vpic.nhtsa.dot.gov/api/vehicles/GetModelsForMakeIdYear/makeId/${makeId}/modelyear/${year}?format=json`,
+  );
+  const data = await res.json();
+
+  return data;
+}
+
+export { getVehicles, getModelsForMakeIdYear };
--- src/types/index.ts
+++ src/types/index.ts
@@ -5,11 +5,28 @@ interface Vehicle {
   VehicleTypeName: string;
 }
 
-interface VehicleResponse {
+interface ApiResponse<Tdata> {
   Count: number;
   Message: string;
   SearchCriteria: string;
-  Results: Vehicle[];
+  Results: Tdata[];
 }
 
-export type { VehicleResponse, Vehicle };
+interface modelsForMakeIdYearRequestParams {
+  makeId: string;
+  year: string;
+}
+
+interface VehicleModel {
+  Make_ID: number;
+  Make_Name: string;
+  Model_ID: number;
+  Model_Name: string;
+}
+
+export type {
+  ApiResponse,
+  Vehicle,
+  modelsForMakeIdYearRequestParams,
+  VehicleModel,
+};
