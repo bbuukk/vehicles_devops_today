7e224f14ea23161ebf2f91348fd97abfd54f3689
Bohdan Buchok <bohdanbuchok@gmail.com>
2024-12-11 16:34:23 +0200

    feat: introduce hook for fetching vehicle models

---
+++ src/hooks/useGetVehicleModels.tsx
@@ -0,0 +1,24 @@
+import { getModelsForMakeIdYear } from "@/api";
+import { modelsForMakeIdYearRequestParams, VehicleModel } from "@/types";
+
+export interface ModelsData {
+  models: VehicleModel[];
+  error: string | null;
+}
+
+export async function getVehicleModels(
+  params: modelsForMakeIdYearRequestParams,
+): Promise<ModelsData> {
+  try {
+    const { Results } = await getModelsForMakeIdYear(params);
+    return {
+      models: Results,
+      error: null,
+    };
+  } catch (err) {
+    return {
+      models: [],
+      error: "Failed to fetch vehicle models. Please try again later.",
+    };
+  }
+}
